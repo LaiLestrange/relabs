bb3c05a96aa77aede0b2aa4af13868ddda52655f
Lais Almeida <laisdev25@gmail.com>
2024-08-16 06:11:14 -0300

    ajusta numero da porta

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -5,5 +5,5 @@ threads threads_count, threads_count
 preload_app!
 
 rackup      File.expand_path('config.ru', __dir__)
-port        ENV['PORT']     || 4000
+port        ENV['PORT']     || 4422
 environment ENV['RACK_ENV'] || 'development'
